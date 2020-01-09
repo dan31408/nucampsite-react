be9028c31785f2dae7b37f68608fb1474038ddc5
Dan Beto <dan.beto31408@gmail.com>
2020-01-09 15:21:24 -0600

    Functional Components

--- src/components/directorycomponents.js
+++ src/components/directorycomponents.js
@@ -1,46 +1,24 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardText, CardImgOverlay, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
-    }
-
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+function RenderDirectoryItem({campsite, onClick}) {
+    return  (
+        <Card onClick={() => onClick(campsite.id)}>                
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
                         </CardImgOverlay>
                     </Card>
-                    </Card>
+    );
+}
+
+function Directory(props) {
+
+   
+        const directory = props.campsites.map(campsite => {
+            return (
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
                 </div>
             );
         });
@@ -55,6 +33,6 @@ class Directory extends Component {
                 
         );
     }
-}
+
 
 export default Directory;
